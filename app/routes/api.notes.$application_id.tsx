bde72c8b655743080d416fe05b71369712dc3a20
Davidjustice28 <david.m.justice1@gmail.com>
2024-06-29 01:30:06 -0600

    sort notes by most recent

--- app/routes/api.notes.$application_id.tsx
+++ app/routes/api.notes.$application_id.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { ApplicationNote, PrismaClient } from "@prisma/client";
 import { ActionFunction, LoaderFunction } from "@remix-run/node";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
@@ -15,7 +15,32 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     },
   });
 
-  return new Response(JSON.stringify({ notes }), {
+  const sorted = mergeSort([...notes]);
+  function mergeSort(arr: ApplicationNote[]): ApplicationNote[] {
+    if (arr.length <= 1) return arr;
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+  function merge(left: ApplicationNote[], right: ApplicationNote[]): ApplicationNote[] {
+    let resultArray = [], leftIndex = 0, rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex].id > right[rightIndex].id) {
+        resultArray.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        resultArray.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+    return resultArray
+      .concat(left.slice(leftIndex))
+      .concat(right.slice(rightIndex));
+  }
+
+  return new Response(JSON.stringify({ notes: sorted }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
