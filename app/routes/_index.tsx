7388601eabc80a33bc42dafbe44ab4d049312ea4
Davidjustice28 <david.m.justice1@gmail.com>
2024-07-10 00:09:27 -0600

    allow logging in in prod

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -62,12 +62,10 @@ export default function LandingPage() {
           <a className="text-sm font-normal text-dark-grey-700 hover:text-dark-grey-900" href="#contact-us">Contact Us</a>
         </div>
         <div className="items-center hidden gap-8 md:flex">
-          {
-            isLocalHost && 
-            (<SignInButton mode="modal" forceRedirectUrl={'/portal/dashboard'} signUpForceRedirectUrl={null} signUpFallbackRedirectUrl={null}>
-              <button className="flex items-center text-sm font-normal text-gray-800 hover:text-gray-900 transition duration-300">Log In</button>
-            </SignInButton>)
-          }
+          
+          (<SignInButton mode="modal" forceRedirectUrl={'/portal/dashboard'} signUpForceRedirectUrl={null} signUpFallbackRedirectUrl={null}>
+            <button className="flex items-center text-sm font-normal text-gray-800 hover:text-gray-900 transition duration-300">Log In</button>
+          </SignInButton>)
           
           {/* <SignUpButton mode="modal" forceRedirectUrl={'/portal/setup'}> */}
             <PLBasicButton text="Book Demo" rounded colorClasses="bg-orange-200 text-orange-600 hover:bg-orange-500 hover:text-white" noDefaultDarkModeStyles={true} onClick={() => window.open('https://cal.com/productlamb/15min', '_blank')}/>
