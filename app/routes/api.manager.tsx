a66c43845e8a8b9d3a5d35f4511428201a1a7ba9
Davidjustice28 <david.m.justice1@gmail.com>
2024-08-24 14:07:04 -0600

    fix audio parsing

--- app/routes/api.manager.tsx
+++ app/routes/api.manager.tsx
@@ -11,29 +11,6 @@ ffmpeg.setFfmpegPath(ffmpegPath!);
 
 
 export const action: ActionFunction = async (args) => {
-  let speech: SpeechClient
-  const config = {
-    type: "service_account",
-    project_id: "product-lamb",
-    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-    private_key: process.env.GCP_PRIVATE_KEY,
-    client_email: process.env.GCP_CLIENT_EMAIL,
-    client_id: process.env.GCP_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
-    universe_domain: "googleapis.com"
-  }
-  console.log('### config: ', config)
-  try {
-    speech = new SpeechClient({
-      credentials: config,
-    });
-  } catch (e) {
-    console.error('### SpeechClient setup error: ', e)
-    return json({ error: 'Unable to process audio.' }, { status: 500 });
-  }
   const request = args.request
   const baseUrl = process.env.SERVER_ENVIRONMENT === 'production' ? process.env.SPRINT_MANAGER_URL_PROD : process.env.SPRINT_MANAGER_URL_DEV
   const cookies = request.headers.get('Cookie')
@@ -54,71 +31,44 @@ export const action: ActionFunction = async (args) => {
     return json({ message: 'No file uploaded.' }, { status: 400 });
   }
 
-  // Convert Blob to Buffer and then to Base64 string
-  let buffer: Buffer
-  let audioBytes: string
-  let metadata: IAudioMetadata
-  try {
-    buffer = Buffer.from(await file.arrayBuffer());
-    // audioBytes = buffer.toString('base64');
-    const { audioBytes: convertedAudioBytes, metadata: parsedMetadata } = await processAudioFile(buffer);
-    audioBytes = convertedAudioBytes;
-    metadata = parsedMetadata;
-  } catch (e) {
-    console.error('Error converting audio file to base64:', e);
-    return json({ message: 'Looks like I was unable to analyze your request. Pleast try again.' }, { status: 500 });
-  }
-
-  function calculateEncoding(codec?: string) {
-    if (!codec) return 'OGG_OPUS'
-    if (codec.toLowerCase().includes('opus')) {
-      console.log('### codec: ', codec)
-      return 'OGG_OPUS'
-    }
-    if (codec === 'MP3') return 'MP3'
-    if (codec === 'MPEG') return 'MP3'
-    return 'OGG_OPUS'
-  }
-  const updatedEncoding = calculateEncoding(metadata.format?.codec)
-  console.log('### updatedEncoding: ', updatedEncoding)
-  try {
-    // Configure the request
-    const requestConfig = {
-      audio: {
-        content: audioBytes,
-      },
-      config: {
-        encoding: updatedEncoding as any, // Make sure this matches your audio file's encoding
-        sampleRateHertz: metadata.format?.sampleRate || 48000, // Adjust this based on your audio file
-        languageCode: 'en-US', // Change as needed
-      },
-    };
-
-    // Initialize the Speech-to-Text client
-    let client: SpeechClient
-    try {
-      
-    } catch (e) {
-      console.error('### SpeechClient setup error: ', e)
-      return json({ error: 'Unable to process audio.' }, { status: 500 });
-    }
-
-    // Call the Speech-to-Text API
-    const [response] = await speech.recognize(requestConfig);
-
-    // Check and handle the response
-    if (!response.results || response.results.length === 0) {
-      console.error('No transcription results found: ', response);
-      return json({ error: 'No transcription results found' }, { status: 400 });
-    }
-
-    const transcript = response.results
-      .map(result => {
-        if (!result.alternatives) return '';
-        return result.alternatives[0].transcript;
-      })
-      .filter(v => v?.length)
-      .join('\n');
+ // Convert Blob to Buffer and then to Base64 string
+ const buffer = Buffer.from(await file.arrayBuffer());
+ const audioBytes = buffer.toString('base64');
+ const { parseBuffer } = await import('music-metadata');
+ const metadata = await parseBuffer(buffer)
+ console.log('audio files metadata: ', JSON.stringify({
+   encoding: metadata.format?.codec,
+   sampleRate: metadata.format?.sampleRate,
+ }, null, 2))
+ try {
+   // Configure the request
+   const requestConfig = {
+     audio: {
+       content: audioBytes,
+     },
+     config: {
+       encoding:  metadata.format?.codec || 'OPUS' as any, // Make sure this matches your audio file's encoding
+       sampleRateHertz: metadata.format?.sampleRate || 48000, // Adjust this based on your audio file
+       languageCode: 'en-US', // Change as needed
+     },
+   };
+   // Initialize the Speech-to-Text client
+   const client = new SpeechClient({
+     credentials: JSON.parse(process.env.GCP_CREDENTIALS || "{}"),
+   });
+   // Call the Speech-to-Text API
+   const [response] = await client.recognize(requestConfig);
+   // Check and handle the response
+   if (!response.results || response.results.length === 0) {
+     return json({ error: 'No transcription results found' }, { status: 400 });
+   }
+   const transcript = response.results
+     .map(result => {
+       if (!result.alternatives) return '';
+       return result.alternatives[0].transcript;
+     })
+     .filter(v => v?.length)
+     .join('\n');  
 
     const iv = process.env.ENCRYPTION_IV as string
     const key = process.env.ENCRYPTION_KEY as string
@@ -219,3 +169,14 @@ async function processAudioFile(buffer: Buffer): Promise<{ audioBytes: string; m
 
   return { audioBytes, metadata };
 };
+
+function calculateEncoding(codec?: string) {
+  if (!codec) return 'OGG_OPUS'
+  if (codec.toLowerCase().includes('opus')) {
+    console.log('### codec: ', codec)
+    return 'OGG_OPUS'
+  }
+  if (codec === 'MP3') return 'MP3'
+  if (codec === 'MPEG') return 'MP3'
+  return 'OGG_OPUS'
+}
